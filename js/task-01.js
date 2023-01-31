3c8a24fe4cab7a51d498987e214ad101269fe963
Sergio Nezhigay <abc1971h@gmail.com>
2023-01-31 12:57:22 +0200

    task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,10 @@
-console.log('task 01 started...');
+console.log('Task 01.');
 
 const categoriesList = document.querySelectorAll('li.item');
 
-console.log('Кількість категорій в ul#categories = ', categoriesList.length);
+console.log('Number of categories: ', categoriesList.length);
+
+categoriesList.forEach(category => {
+  console.log('\nCategory: ', category.firstElementChild.textContent); // Header
+  console.log('Elements: ', category.lastElementChild.children.length); // ul list elements q-ty
+});
