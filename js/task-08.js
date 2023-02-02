29202975f3b8e15e0f5085b5d51b8e29c206a1de
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-02 15:55:59 +0200

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
+const formLoginFormEl = document.querySelector('form.login-form');
 
+formLoginFormEl.addEventListener('submit', onFormLoginSubmit);
+
+function onFormLoginSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.target;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+    return console.log('Please fill in all the fields!');
+  }
+  const result = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log('Form data object: ', result);
+  event.currentTarget.reset();
+}
+
+// function onFormLoginSubmit(event) {
+//     event.preventDefault();
+
+//     var formData = new FormData(event.target);
+//     for (let pair of formData.entries()) {
+//       console.log(pair[0] + ': ' + pair[1]);
+//       if (!pair[1]) {
+//         // Checking if the filed value is empty
+//         alert('Please fill in all the fields!');
+//         return console.log('Please fill in all the fields!');
+//       }
+//     }
+//     event.currentTarget.reset();
+//   }
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonChangeColorEl = document.querySelector('button.change-color');
+const spanColorEl = document.querySelector('span.color');
+const bodyEl = document.querySelector('body');
+
+buttonChangeColorEl.addEventListener('click', onButtonChangeColorClick);
+
+function onButtonChangeColorClick() {
+  const randomHexColor = getRandomHexColor();
+  spanColorEl.textContent = randomHexColor;
+  bodyEl.style.backgroundColor = randomHexColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
