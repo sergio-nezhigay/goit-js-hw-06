84521e53c54f57c0d214f1475e7bf8145f8b70e0
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-02 16:53:05 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
+const createButtonEl = document.querySelector('button[data-create]');
+const destroyButtonEl = document.querySelector('button[data-destroy]');
+const divBoxesEl = document.querySelector('div#boxes');
+const inputNumberEl = document.querySelector('input[type="number"]');
+
+createButtonEl.addEventListener('click', createBoxes);
+destroyButtonEl.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  divBoxesEl.innerHTML = '';
+}
+
+function createBoxes() {
+  const numberOfBoxes = inputNumberEl.value;
+  if (!numberOfBoxes) {
+    alert('You need to choose number of boxes first...');
+    return;
+  }
+  let boxesString = '';
+  for (let size = 30, i = 0; i < numberOfBoxes; i++, size += 10) {
+    boxesString += `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px"></div>\n`;
+  }
+  divBoxesEl.insertAdjacentHTML('beforeend', boxesString);
+  inputNumberEl.value = 0; /* reset old Box number value */
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
